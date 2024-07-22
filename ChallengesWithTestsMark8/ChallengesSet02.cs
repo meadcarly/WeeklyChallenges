d777579692359c9f22004178183521d9dab92cd2
Carly Mead <meadcarly16@gmail.com>
2024-07-22 13:23:45 -0600

    Completed challenge set 1, 2 and 3

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +9,150 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            return char.IsLetter(c);
         }
 
-        public bool CountOfElementsIsEven(string[] vals)
+    public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            int count = vals.Length;
+            if (count % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if(number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            if (num % 2 == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || !numbers.Any())
+            {
+                return 0;
+            }
+            
+            double max = numbers.Max();
+            double min = numbers.Min();
+            
+            return min + max;
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if (str1.Length < str2.Length)
+            {
+                return str1.Length;
+            }
+            else
+            {
+                return str2.Length;
+            }
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            if (numbers != null)
+            {
+                foreach (int num in numbers)
+                {
+                    sum += num;
+                }
+            }
+            else
+            {
+                sum = 0;
+            }
+            return sum;
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            int sumEven = 0;
+            if (numbers != null)
+            {
+                foreach (int num in numbers)
+                {
+                    if (num % 2 == 0)
+                    {
+                        sumEven += num;
+                    }
+                    else
+                    {
+                        sumEven += 0;
+                    }
+                }
+            }
+            else
+            {
+                sumEven = 0;
+            }
+            return sumEven;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            int sumIsOdd = 0;
+            if (numbers != null)
+            {
+                foreach (int numb in numbers)
+                {
+                    sumIsOdd += numb;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+            if (sumIsOdd % 2 == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            long count = 0;
+            if (number > 0)
+            {
+                count = number / 2;
+            }
+            else
+            {
+                count = 0;
+            }
+
+            return count;
         }
     }
 }
--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -7,47 +7,120 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            foreach (bool val in vals)
+            {
+                if (!val)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            if (numbers == null)
+            {
+                return false;
+            }
+            foreach (int number in numbers)
+            {
+                if (number % 2 != 0)
+                {
+                    sum += number;
+                }
+            }
+            
+            if (sum % 2 == 0)
+            {
+                return false;
+            }
+                
+            return true;
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            var hasUpper = false;
+            var hasLower = false;
+            var hasNumber = false;
+            foreach (char a in password)
+            {
+                if (char.IsUpper(a))
+                {
+                    hasUpper = true;
+                }
+                else if (char.IsLower(a))
+                {
+                    hasLower = true;
+                }
+                else if (char.IsDigit(a))
+                {
+                    hasNumber = true;
+                }
+
+                if (hasUpper && hasLower && hasNumber)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
-        public char GetFirstLetterOfString(string val)
-        {
-            throw new NotImplementedException();
-        }
+            public char GetFirstLetterOfString(string val)
+            {
+                    char firstLetter = val[0];
+                    return firstLetter;
+            }
 
-        public char GetLastLetterOfString(string val)
-        {
-            throw new NotImplementedException();
-        }
+            public char GetLastLetterOfString(string val)
+            {
+                char lastLetter = val[val.Length - 1];
+                return lastLetter;
+            }
 
-        public decimal Divide(decimal dividend, decimal divisor)
-        {
-            throw new NotImplementedException();
-        }
+            public decimal Divide(decimal dividend, decimal divisor)
+            {
+                if (divisor == 0)
+                {
+                    return 0;
+                } 
+                return dividend / divisor;
+            }
 
-        public int LastMinusFirst(int[] nums)
-        {
-            throw new NotImplementedException();
-        }
+            public int LastMinusFirst(int[] nums)
+            {
+                return nums[nums.Length - 1] - nums[0];
+            }
 
-        public int[] GetOddsBelow100()
-        {
-            throw new NotImplementedException();
-        }
+            public int[] GetOddsBelow100()
+            {
+                int[] oddList = new int[50];
+                int index = 0;
+                for(int i = 0; i < 100; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        oddList[index] = i;
+                        index++;
+                    }
+                }
 
-        public void ChangeAllElementsToUppercase(string[] words)
-        {
-            throw new NotImplementedException();
-        }
+                return oddList;
+            }
+
+           public void ChangeAllElementsToUppercase(string[] words)
+           {
+               for(int i = 0; i < words.Length; i++)
+               {
+                   words[i] = words[i].ToUpper();
+               }
+
+               foreach(string word in words)
+               {
+                   Console.WriteLine(word);
+               }
+           }
     }
 }
