83a31f0b8aa07636f2866cbcae96864002dad173
Carly Mead <meadcarly16@gmail.com>
2024-07-27 19:26:09 -0600

    Finished challenge set 04

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +8,120 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            var addEven = numbers.Where(x => x % 2 == 0).Sum(x => x);
+            var subtractOdd = numbers.Where(x => x % 2 != 0).Sum(x => x);
+            var addEvenSubtractOdd = addEven - subtractOdd;
+            return addEvenSubtractOdd;
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            List<string> allTheStrings = new List<string> { str1, str2, str3, str4 };
+            var lengthOfShortestString = allTheStrings.Min(x => x.Length);
+            return lengthOfShortestString;
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            List<int> allTheIntegers = new List<int> {number1, number2, number3, number4};
+            var smallestNumber = allTheIntegers.Min(x => x);
+            return smallestNumber;
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            var changeBusinessName = "TrueCoders";
+            biz.Name = changeBusinessName;
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            if (sideLength1 <= 0 || sideLength2 <= 0 || sideLength3 <= 0)
+            {
+                return false;
+                
+            }
+            if (!(sideLength1 + sideLength2 > sideLength3))
+            {
+                return false;
+            }
+            if (!(sideLength3 + sideLength2 > sideLength1))
+            {
+                return false;
+            }
+            if (!(sideLength1 + sideLength1 > sideLength2))
+            {
+                return false;
+            }
+
+            return true;
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            double check;
+            if (double.TryParse(input, out check))
+            {
+                return true;
+            }
+
+            return false;
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            var nullTotal = 0;
+            var nonNullTotal = 0;
+            foreach(var input in objs)
+            {
+                if (input == null)
+                {
+                    nullTotal += 1;
+                }
+                else
+                {
+                    nonNullTotal += 1;
+                }
+            }
+            if (nullTotal > nonNullTotal)
+            {
+                return true;
+            }
+            return false;
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+
+            var evens = numbers.Where(x => x % 2 == 0);
+            if (!evens.Any())
+            {
+                return 0;
+            }
+
+            return evens.Average(x => x);
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            var factorialTotal = 1;
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(number));
+            }
+            if (number == 0)
+            {
+                return 1;
+            }
+            for (int i = 1; i <= number; i++)
+            {
+                factorialTotal *= i;
+            }
+            return factorialTotal;
         }
     }
 }
